eff44d55cc06cb0dffa30264dc7182767bd52094
kokeunho <kokangsik65@naver.com>
2025-10-19 05:50:18 +0900

    feat(calculator): 입력 받고 결과값 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,13 +1,58 @@
 package calculator;
 
+import calculator.delimiter.CustomDelimiterExtractor;
+import calculator.delimiter.CustomDelimiterValidator;
+import calculator.parser.NumberSeparator;
+import calculator.parser.NumberValidator;
+
 import java.io.Console;
+import java.util.List;
 
 public class Application {
+
     public static void main(String[] args) {
 
-//        Console console = System.console();
-//        String input = console.readLine();
+        CustomDelimiterValidator customDelimiterValidator = new CustomDelimiterValidator();
+        CustomDelimiterExtractor customDelimiterExtractor = new CustomDelimiterExtractor();
 
-        //TODO: input이 빈 문자열일 경우 값 0 출력
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        Console console = System.console();
+        String input = console.readLine();
+
+        int result = 0;
+        String numberPart = input;
+        String delimiters = ",:";
+
+        // 커스텀 구분자 구문이 있는 경우
+        if (input.startsWith("//")) {
+            int customDelimiterEndIdx = input.indexOf("\\n");
+
+            if (customDelimiterEndIdx == -1) {
+                throw new IllegalArgumentException("잘못된 커스텀 구분자 구문입니다.");
+            }
+
+            String customDelimiterStr = input.substring(0, customDelimiterEndIdx+2);
+
+            customDelimiterValidator.validate(customDelimiterStr);
+
+            String customDelimiter = customDelimiterExtractor.extractCustomDelimiter(customDelimiterStr);
+
+            delimiters += customDelimiter;
+
+            numberPart = input.substring(customDelimiterEndIdx + 2);
+        }
+
+        NumberValidator numberValidator = new NumberValidator(delimiters);
+        NumberSeparator numberSeparator = new NumberSeparator(delimiters);
+
+        numberValidator.validate(numberPart);
+
+        List<Integer> numbers = numberSeparator.getNumbers(numberPart);
+
+        for (int number : numbers) {
+            result += number;
+        }
+
+        System.out.println("결과 : " + result);
     }
 }
