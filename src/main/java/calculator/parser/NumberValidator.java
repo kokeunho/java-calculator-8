d82b06f1a876d90a88521efd838e49cb6da7d7db
kokeunho <kokangsik65@naver.com>
2025-10-18 18:54:18 +0900

    refactor(parser): 특수문자 escape 하는 메서드 대신 Pattern.quote 사용

--- src/main/java/calculator/parser/NumberValidator.java
+++ src/main/java/calculator/parser/NumberValidator.java
@@ -16,7 +16,7 @@ public class NumberValidator implements Validator<String> {
     public void validate(String input) {
 
         String numberPart = input.substring(input.indexOf("\\n") + 2);
-        String safeDelimiters = escapeDelimiter(delimiters);
+        String p = Pattern.quote(delimiters);
 
         // 숫자 문자열이 없을 때
         if (numberPart.isEmpty()) return;
@@ -27,40 +27,24 @@ public class NumberValidator implements Validator<String> {
         }
 
         // 연속된 구분자가 있을 때
-        String continuousPattern = "[" + safeDelimiters + "]{2,}";
+        String continuousPattern = "[" + p + "]{2,}";
         if (Pattern.compile(continuousPattern).matcher(numberPart).find()) {
             throw new IllegalArgumentException("구분자는 연속으로 입력할 수 없습니다.");
         }
 
         // 사용 불가능한 구분자 있을 때
-        String invalidPattern = "[^0-9" + safeDelimiters + "-]";
+        String invalidPattern = "[^0-9" + p + "-]";
         if (Pattern.compile(invalidPattern).matcher(numberPart).find()) {
             throw new IllegalArgumentException("허용되지 않은 문자가 포함되어 있습니다.");
         }
 
         // 음수가 있을
-        String[] tokens = numberPart.split("[" + safeDelimiters + "]");
+        String[] tokens = numberPart.split("[" + p + "]");
         for (String token : tokens) {
             int num = Integer.parseInt(token);
             if (num < 0) {
                 throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
             }
         }
-    }
-
-    // 정규표현식에서 특수문자를 사용하기 위해 escape
-    private String escapeDelimiter(String s) {
-
-        StringBuilder sb = new StringBuilder();
-
-        for (char c : s.toCharArray()) {
-            if ("\\-^[]".indexOf(c) != -1) {
-                sb.append("\\").append(c);
-            } else {
-                sb.append(c);
-            }
-        }
-
-        return sb.toString();
     }
 }
