23313e27955824b8e1cbba897a0d2a497c0f7a7b
kokeunho <kokangsik65@naver.com>
2025-10-19 23:50:41 +0900

    fix(calculate): 빈 입력 ""이 들어왔을 때, 결과값 0을 출력하도록 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -24,30 +24,33 @@ public class Application {
         String delimiters = ",:";
 
         // 커스텀 구분자 구문이 있는 경우
-        if (input.startsWith("//")) {
-            customDelimiterValidator.validate(input);
+        if (!input.isEmpty()){
+            if (input.startsWith("//")) {
+                customDelimiterValidator.validate(input);
 
-            String customDelimiterStr = input.substring(0, input.indexOf("\\n") + 2);
+                String customDelimiterStr = input.substring(0, input.indexOf("\\n") + 2);
 
-            customDelimiterValidator.validate(customDelimiterStr);
+                customDelimiterValidator.validate(customDelimiterStr);
 
-            String customDelimiter = customDelimiterExtractor.extractCustomDelimiter(customDelimiterStr);
+                String customDelimiter = customDelimiterExtractor.extractCustomDelimiter(customDelimiterStr);
 
-            delimiters += customDelimiter;
+                delimiters += customDelimiter;
 
-            numberPart = input.substring(input.indexOf("\\n") + 2);
+                numberPart = input.substring(input.indexOf("\\n") + 2);
+            }
+
+            NumberValidator numberValidator = new NumberValidator(delimiters);
+            NumberSeparator numberSeparator = new NumberSeparator(delimiters);
+
+            numberValidator.validate(numberPart);
+
+            List<Integer> numbers = numberSeparator.getNumbers(numberPart);
+
+            for (int number : numbers) {
+                result += number;
+            }
         }
 
-        NumberValidator numberValidator = new NumberValidator(delimiters);
-        NumberSeparator numberSeparator = new NumberSeparator(delimiters);
-
-        numberValidator.validate(numberPart);
-
-        List<Integer> numbers = numberSeparator.getNumbers(numberPart);
-
-        for (int number : numbers) {
-            result += number;
-        }
 
         System.out.println("결과 : " + result);
     }
